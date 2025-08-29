b940575e3d30ce0af59b4d9ece9d6896c0a8ce06
Leandro Parazito <leandroh@gmail.com>
2025-08-29 16:41:15 -0300

    refactor: remove unused code

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -42,20 +42,6 @@ install_kubectl() {
   echo "✅ kubectl installed ($(kubectl version --client --short 2>/dev/null || true))"
 }
 
-add_docker_repo() {
-  sudo apt-get update
-  sudo apt-get install ca-certificates curl
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-  sudo apt-get update
-}
-
 install_docker() {
   if command -v docker &>/dev/null; then
     return
