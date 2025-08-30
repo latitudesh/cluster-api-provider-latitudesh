ba7efcf956f396a1048e4940707fac7ac34a793a
Pery Lemke <3199483+perylemke@users.noreply.github.com>
2025-08-30 09:13:17 -0300

    feat: implement types

--- api/v1beta1/latitudecluster_types.go
+++ api/v1beta1/latitudecluster_types.go
@@ -11,22 +11,37 @@ type APIEndpoint struct {
 }
 
 type ProjectRef struct {
+	// ProjectID is the Latitude.sh project ID
 	ProjectID string `json:"projectID,omitempty"`
 }
 
 type LatitudeClusterSpec struct {
-	Location   string      `json:"location,omitempty"`
+	// Location is the Latitude.sh region/site where resources will be created
+	Location string `json:"location,omitempty"`
+
+	// ProjectRef is the Latitude.sh project reference
 	ProjectRef *ProjectRef `json:"projectRef,omitempty"`
+
+	// ControlPlaneEndpoint represents the endpoint used to communicate with the control plane
+	ControlPlaneEndpoint APIEndpoint `json:"controlPlaneEndpoint,omitempty"`
 }
 
 type LatitudeClusterStatus struct {
-	Ready                bool        `json:"ready,omitempty"`
+	// Ready indicates that the cluster infrastructure is ready
+	Ready bool `json:"ready,omitempty"`
+
+	// ControlPlaneEndpoint represents the endpoint used to communicate with the control plane
 	ControlPlaneEndpoint APIEndpoint `json:"controlPlaneEndpoint,omitempty"`
+
+	// Conditions defines current service state of the LatitudeCluster
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=latitudeclusters,scope=Namespaced,shortName=latc
+// +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.ready"
+// +kubebuilder:printcolumn:name="Endpoint",type="string",JSONPath=".status.controlPlaneEndpoint.host"
 type LatitudeCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -44,4 +59,15 @@ type LatitudeClusterList struct {
 
 func init() {
 	SchemeBuilder.Register(&LatitudeCluster{}, &LatitudeClusterList{})
+}
+
+// GetConditions returns the set of conditions for this object
+func (c *LatitudeCluster) GetConditions() []metav1.Condition {
+	return c.Status.Conditions
+}
+
+// SetConditions sets the conditions for this object
+func (c *LatitudeCluster) SetConditions(conditions []metav1.Condition) {
+	c.Status.Conditions = conditions
+
 }
--- api/v1beta1/latitudemachine_types.go
+++ api/v1beta1/latitudemachine_types.go
@@ -6,21 +6,55 @@ import (
 
 type LatitudeMachineSpec struct {
 	// +kubebuilder:validation:MinLength=1
+	// OperatingSystem is the operating system slug for the server
 	OperatingSystem string `json:"operatingSystem"`
 
 	// +kubebuilder:validation:MinLength=1
+	// Plan is the server plan slug
 	Plan string `json:"plan"`
+
+	// Site is the Latitude.sh site/region where the server will be deployed
+	Site string `json:"site,omitempty"`
+
+	// ProjectID is the Latitude.sh project ID
+	ProjectID string `json:"projectID,omitempty"`
+
+	// SSHKeys is a list of SSH key IDs to be installed on the server
+	SSHKeys []string `json:"sshKeys,omitempty"`
+
+	// UserData is cloud-init user data to be applied to the server
+	UserData string `json:"userData,omitempty"`
 }
 
 type LatitudeMachineStatus struct {
+	// Ready indicates that the machine is ready
 	// +optional
 	Ready bool `json:"ready,omitempty"`
 
+	// ProviderID is the provider ID of the server
 	// +optional
 	ProviderID string `json:"providerID,omitempty"`
 
+	// ServerID is the Latitude.sh server ID
+	// +optional
+	ServerID string `json:"serverID,omitempty"`
+
+	// Addresses contains the addresses associated with the machine
+	// +optional
+	Addresses []MachineAddress `json:"addresses,omitempty"`
+
+	// Conditions defines current service state of the LatitudeMachine
 	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// MachineAddress contains information about a machine's address
+type MachineAddress struct {
+	// Type of the address (e.g. InternalIP, ExternalIP)
+	Type string `json:"type"`
+
+	// Address is the actual IP address or hostname
+	Address string `json:"address"`
 }
 
 // +kubebuilder:object:root=true
@@ -28,6 +62,7 @@ type LatitudeMachineStatus struct {
 // +kubebuilder:resource:path=latitudemachines,scope=Namespaced,shortName=latm
 // +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=".status.ready"
 // +kubebuilder:printcolumn:name="ProviderID",type=string,JSONPath=".status.providerID"
+// +kubebuilder:printcolumn:name="ServerID",type=string,JSONPath=".status.serverID"
 type LatitudeMachine struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -46,3 +81,34 @@ type LatitudeMachineList struct {
 func init() {
 	SchemeBuilder.Register(&LatitudeMachine{}, &LatitudeMachineList{})
 }
+
+// GetConditions returns the set of conditions for this object
+func (m *LatitudeMachine) GetConditions() []metav1.Condition {
+	return m.Status.Conditions
+}
+
+// SetConditions sets the conditions for this object
+func (m *LatitudeMachine) SetConditions(conditions []metav1.Condition) {
+	m.Status.Conditions = conditions
+}
+
+// Condition types and reasons
+const (
+	// InstanceReadyCondition reports on current status of the instance
+	InstanceReadyCondition = "InstanceReady"
+
+	// InstanceProvisionFailedReason used when the instance couldn't be created
+	InstanceProvisionFailedReason = "InstanceProvisionFailed"
+
+	// InstanceNotReadyReason used when the instance is not ready
+	InstanceNotReadyReason = "InstanceNotReady"
+
+	// InstanceDeletionFailedReason used when the instance couldn't be deleted
+	InstanceDeletionFailedReason = "InstanceDeletionFailed"
+
+	// WaitingForClusterInfrastructureReason used when machine is waiting for cluster infrastructure
+	WaitingForClusterInfrastructureReason = "WaitingForClusterInfrastructure"
+
+	// WaitingForBootstrapDataReason used when machine is waiting for bootstrap data
+	WaitingForBootstrapDataReason = "WaitingForBootstrapData"
+)
